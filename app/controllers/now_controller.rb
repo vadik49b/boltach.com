04166e4ee32cbb88fd2cb8cb03c220e2f6d8ea06
Vadim Boltach <vadim@boltach.com>
2026-01-09 16:48:11 -0300

    Generate auth

--- app/controllers/now_controller.rb
+++ app/controllers/now_controller.rb
@@ -1,4 +1,6 @@
 class NowController < ApplicationController
+  allow_unauthenticated_access
+
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   get "now/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
